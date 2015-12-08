28b8818e033b7fa5341a77b53634b9ef1fc5c584
Sahri Riza Umami <riza@openmailbox.org>
2015-12-09 02:43:10 +0700

    Fix permissions and simplified the code.

--- plugins/greeter.lua
+++ plugins/greeter.lua
@@ -26,38 +26,12 @@ local function is_super_banned(user_id)
     return superbanned or false
 end
 
-local function welcome_message(msg, new_member)
-
-  local data = load_data(_config.moderation.data)
-  local welcome_stat = data[tostring(msg.to.id)]['settings']['welcome']
-
-  if data[tostring(msg.to.id)] then
-    local about = ''
-    local rules = ''
-    if data[tostring(msg.to.id)]['description'] then
-      about = data[tostring(msg.to.id)]['description']
-      about = "\nDescription :\n"..about.."\n"
-    end
-    if data[tostring(msg.to.id)]['rules'] then
-      rules = data[tostring(msg.to.id)]['rules']
-      rules = "\nRules :\n"..rules.."\n"
-    end
-    local welcomes = "Welcome "..new_member..".\nYou are in group '"..string.gsub(msg.to.print_name, "_", " ").."'\n"
-    if welcome_stat == 'group' then
-      receiver = get_receiver(msg)
-    elseif welcome_stat == 'private' then
-      receiver = 'user#id'..msg.from.id
-    end
-    send_large_msg(receiver, welcomes..about..rules.."\n", ok_cb, false)
-  end
-end
-
 local function run(msg, matches)
 
   local data = load_data(_config.moderation.data)
   local welcome_stat = data[tostring(msg.to.id)]['settings']['welcome']
 
-  if matches[1] == 'welcome' then
+  if matches[1] == 'welcome' and is_mod(msg) then
     if matches[2] == 'group' then
       if welcome_stat ~= 'group' then
         data[tostring(msg.to.id)]['settings']['welcome'] = 'group'
@@ -84,7 +58,6 @@ local function run(msg, matches)
   end
 
   if welcome_stat ~= 'no' and msg.action and msg.action.type then
-    -- do not greet (super)banned users or API bots.
     local action = msg.action.type
     if action == 'chat_add_user' or action == 'chat_add_user_link' or action == "chat_del_user" then
       if msg.action.link_issuer then
@@ -96,6 +69,7 @@ local function run(msg, matches)
         new_member = (msg.action.user.first_name or '')..' '..(msg.action.user.last_name or '')
         user_flags = msg.action.user.flags
       end
+      -- do not greet (super)banned users or API bots.
       local superbanned = is_super_banned(user_id)
       local banned = is_banned(user_id, msg.to.id)
       if superbanned or banned then
@@ -103,20 +77,35 @@ local function run(msg, matches)
         return nil
       end
       if user_flags == 4352 then
-        print "Ignored. It's an API bot."
+        print 'Ignored. It is an API bot.'
         return nil
       end
     end
 
-    if matches[1] == "chat_add_user" then
-      welcome_message(msg, new_member)
-    elseif matches[1] == "chat_add_user_link" then
-      welcome_message(msg, new_member)
-    elseif matches[1] == "chat_del_user" then
+    if matches[1] == 'chat_add_user' or matches[1] == 'chat_add_user_link' then
+      if data[tostring(msg.to.id)] then
+        local about = ''
+        local rules = ''
+        if data[tostring(msg.to.id)]['description'] then
+          about = data[tostring(msg.to.id)]['description']
+          about = '\nDescription :\n'..about..'\n'
+        end
+        if data[tostring(msg.to.id)]['rules'] then
+          rules = data[tostring(msg.to.id)]['rules']
+          rules = '\nRules :\n'..rules..'\n'
+        end
+        local welcomes = 'Welcome '..new_member..'\nYou are in group '..msg.to.title..'\n'
+        if welcome_stat == 'group' then
+          receiver = get_receiver(msg)
+        elseif welcome_stat == 'private' then
+          receiver = 'user#id'..msg.from.id
+        end
+        send_large_msg(receiver, welcomes..about..rules..'\n', ok_cb, false)
+      end
+    elseif matches[1] == 'chat_del_user' then
       return 'Bye '..new_member..'!'
     end
   end
-
 end
 
 return {
@@ -132,8 +121,7 @@ return {
     "^!!tgservice (.+)$",
     "^!(welcome) (.*)$"
   },
-  run = run,
-  moderated = true
+  run = run
 }
 
 end
