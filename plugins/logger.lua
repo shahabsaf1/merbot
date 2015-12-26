fe54e9447b127ff670a3eac78bd530772e7a06c7
Sahri Riza Umami <riza@openmailbox.org>
2015-12-26 17:24:08 +0700

    Add logger plugin.

---
+++ plugins/logger.lua
@@ -0,0 +1,40 @@
+do
+
+  function run(msg, matches)
+    if is_chat_msg(msg) then
+      local log_file = './data/log/'..msg.to.id..'_log.csv'
+      local logtxt = os.date('%F;%T', msg.date)..';'..msg.to.title..';'..msg.to.id
+                     ..';'..(msg.from.first_name or '')..(msg.from.last_name or '')
+                     ..';@'..(msg.from.username or '')..';'..msg.from.id..';'
+                     ..msg.text..'\n'
+      local file = io.open(log_file, 'a')
+
+      file:write(logtxt)
+      file:close()
+
+      if is_mod(msg) then
+        if matches[1] == 'get' then
+          send_document('chat#id'..msg.to.id, log_file, ok_cb, false)
+        elseif matches[1] == 'pm' then
+          send_document('user#id'..msg.from.id, log_file, ok_cb, false)
+        end
+      end
+    end
+  end
+
+  return {
+    description = 'Logging group messages.',
+    usage = {
+      '!log get : Send chat log to its chat group',
+      '!log pm : Send chat log to private message'
+      },
+    patterns = {
+      '^!log (get)$',
+      '^!log (pm)$',
+      '^.+$'
+    },
+    run = run
+  }
+
+end
+
