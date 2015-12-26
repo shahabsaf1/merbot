1fe880d6853658374cb5b159710faf72cdb4f156
Sahri Riza Umami <riza@openmailbox.org>
2015-12-26 19:11:04 +0700

    Fix typo. Log supposed to be logs. I am embarassed my family and need to commit seppuku.

--- plugins/logger.lua
+++ plugins/logger.lua
@@ -1,23 +1,24 @@
 do
 
-  function run(msg, matches)
+  local function pre_process(msg)
     if is_chat_msg(msg) then
-      local log_file = './data/log/'..msg.to.id..'_log.csv'
       local logtxt = os.date('%F;%T', msg.date)..';'..msg.to.title..';'..msg.to.id
                      ..';'..(msg.from.first_name or '')..(msg.from.last_name or '')
                      ..';@'..(msg.from.username or '')..';'..msg.from.id..';'
                      ..msg.text..'\n'
-      local file = io.open(log_file, 'a')
-
+      local file = io.open('./data/logs/'..msg.to.id..'_log.csv', 'a')
       file:write(logtxt)
       file:close()
+    end
+    return msg
+  end
 
-      if is_mod(msg) then
-        if matches[1] == 'get' then
-          send_document('chat#id'..msg.to.id, log_file, ok_cb, false)
-        elseif matches[1] == 'pm' then
-          send_document('user#id'..msg.from.id, log_file, ok_cb, false)
-        end
+  function run(msg, matches)
+    if is_mod(msg) then
+      if matches[1] == 'get' then
+        send_document('chat#id'..msg.to.id, './data/logs/'..msg.to.id..'_log.csv', ok_cb, false)
+      elseif matches[1] == 'pm' then
+        send_document('user#id'..msg.from.id, './data/logs/'..msg.to.id..'_log.csv', ok_cb, false)
       end
     end
   end
@@ -30,10 +31,10 @@ do
       },
     patterns = {
       '^!log (get)$',
-      '^!log (pm)$',
-      '^.+$'
+      '^!log (pm)$'
     },
-    run = run
+    run = run,
+    pre_process = pre_process
   }
 
 end
